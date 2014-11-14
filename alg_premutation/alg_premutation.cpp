b9a8deef8197adc657d4f912401e69e191634009
xyd <xuyangdee@126.com>
2014-11-14 21:27:56 +0800

    Add codes.

---
+++ alg_premutation/alg_premutation.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include <algorithm>
+#include <deque>
+#include <functional>
+using namespace std;
+template<typename T>
+void p(const T &c)
+{for(typename T::const_iterator iter=c.begin();iter!=c.end();iter++) cout<<*iter<<" "; cout<<endl;}
+template<typename T>
+void initC(T &c,int n)
+{c.clear();for(int i=0;i<n;i++)c.push_back(typename T::value_type(i));}
+template<typename T>
+void addC(T &c,int n)
+{for(int i=0;i<n;i++)c.push_back(typename T::value_type(i));}
+template<typename T>
+void rsort(T &c){sort(c.begin(),c.end(),greater<typename T::value_type>());}
+
+template<typename T>
+void testPartition(T &t){partition(t.begin(),t.end(),not1( bind2nd(modulus<typename T::value_type>(),2)));}
+template<typename T>
+void testNth_elemetn(T &t){
+nth_element(t.begin(),t.begin()+9,t.end());
+}
+
+int main()
+{
+	string s("bca");
+	while(next_permutation(s.begin(),s.end()))
+		cout<<s<<endl;
+	cout<<"end::"<<s<<endl;
+	return 0;
+}
