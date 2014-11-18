093e2f3e92f5418ecf9ecec864089a9065f487d1
xyd <xuyangdee@126.com>
2014-11-18 21:48:57 +0800

    Modify class_handle.

--- class_handle/class_handle.cpp
+++ class_handle/class_handle.cpp
@@ -1,24 +1,44 @@
 #include <cstdio>
 #include <string>
 #include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
 #include "class_handle.h"
 using namespace std;
-
 class A: public class_hdl_base
 {
-int a;
 public:
-A():a(0){cout<<"init A"<<endl;}
-void p(){cout<<"a="<<a<<endl;}
-void add(){a++;p();}
+A(){cout<<"init A"<<endl;}
+A(const A& rhl){cout<<"copy init A"<<endl;}
+virtual void p()const{cout<<"in class A"<<endl;}
 A * clone()const {return new A(*this);}
 ~A(){cout<<"destroy A"<<endl;}
 };
+class B: public A
+{
+public:
+B(){cout<<"init B"<<endl;}
+B(const B& rhl){cout<<"copy init B"<<endl;}
+virtual void p()const{cout<<"in class B"<<endl;}
+B * clone()const {return new B(*this);}
+~B(){cout<<"destroy B"<<endl;}
+};
+typedef vector<class_hdl<A> > vectorA;
+void p(const class_hdl<A> t)
+{
+	t->p();
+}
 int main()
 {
-	A a();
-	class_hdl<A> c;
-	
+	A a;B b;
+	vectorA va;
+	{
+	class_hdl<A> c(a), d(b);
+	va.push_back(c);va.push_back(d);}
+	for_each(va.begin(),va.end(),p);
+	getchar();
+	va.erase(va.begin());
 	getchar();
 	return 0;
 }
--- class_handle/class_handle.h
+++ class_handle/class_handle.h
@@ -8,11 +8,12 @@ class class_hdl
 {
 T *p;
 int *use;
-void decr_use(){if(--*use==0){delete p;delete use;}}
+void decr_use(){if(--*use==0){if(p) delete p;delete use;}}
 public:
 class_hdl():p(0),use(new int(1)){}
 class_hdl(const T &rhl):p(rhl.clone()),use(new int(1)){}
-class_hdl(const class_hdl *rhl):p(rhl.p),use(rhl.use){++*use;}
+class_hdl(const class_hdl &rhl):p(rhl.p),use(rhl.use){++*use;}
+class_hdl &operator=(const class_hdl &rhl){++*rhl.use;decr_use();p=rhl.p;use=rhl.use;return *this;}
 ~class_hdl(){decr_use();}
 
 const T *operator->()const 
